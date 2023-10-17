b62e41142d00c353bbad54dde49301f7ff5e1787
juhi.sinhaa <juhi.gsinha@gmail.com>
2023-10-17 14:17:26 +0530

    2nd commit

--- 16Oct23/src/test/java/stepDefinition/Steps.java
+++ 16Oct23/src/test/java/stepDefinition/Steps.java
@@ -10,17 +10,25 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pageObjects.Login;
+import pageObjects.Login2;
 import pageObjects.Template;
 
 
 public class Steps {
+	WebDriver driver;
 	@Given("I hit the url {string}")
-	public void i_hit_the_url(String string) {
-	  System.out.print("code1");
+	public void i_hit_the_url(String url) {
+		driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		   driver.get(url);
+		   
 	}
 
 	@Given("Enter  all the details")
 	public void enter_all_the_details() {
+		Login2 log=new Login2(driver);
+		log.registerUser();
 		System.out.print("code2");  
 	}
 
